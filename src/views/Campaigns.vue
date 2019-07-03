aa35e9b34f3c3a7654cfac703e4425e3b53cc2a6
Guy Oren <orenguyster@gmail.com>
2019-07-03 22:50:35 +0300

    Commit 3

--- src/views/Campaigns.vue
+++ src/views/Campaigns.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="campaigns">
     <h1>This is an Campaigns page</h1>
-     <CampaignsTable />
+    <CampaignsTable />
   </div>
 </template>
 <script>
-import CampaignsTable from '../components/Campaigns/CampaignsTable'
+import CampaignsTable from "../components/Campaigns/CampaignsTable";
 export default {
-  components:{
-     CampaignsTable
+  components: {
+    CampaignsTable
   }
- 
-  
-}
+};
 </script>
